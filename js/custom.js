0bba13375f7b837e5e6fa1db2594d733ab0f2ff8
Sam Wang <samwang@Sams-MacBook-Air.local>
2020-05-17 23:01:01 -0700

    first template for 2nd page

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+/**
+ * Listen to scroll to change header opacity class
+ */
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("bg-white");
+    }else{
+        $('.navbar').removeClass("bg-white");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+
+/**
+ * Read the background color from an existed element
+ */
+$(document).ready(function() {
+    var $headerDiv = $('#header-block');
+
+    if ( $headerDiv.length > 0){
+        $('nav').css("background-color", $headerDiv.css("background-color"));
+    }
+});
+
+
+/**
+ * Using magnific-popup to zoom image on click
+ */
+$("a[name=image-zoomer]").magnificPopup({
+    type: 'image',
+    closeBtnInside: false,
+closeOnContentClick: false,
+
+callbacks: {
+  open: function() {
+    var self = this;
+    self.wrap.on('click.pinhandler', 'img', function() {
+      self.wrap.toggleClass('mfp-force-scrollbars');
+    });
+  },
+  beforeClose: function() {
+        this.wrap.off('click.pinhandler');
+    this.wrap.removeClass('mfp-force-scrollbars');
+  }
+},
+ 
+image: {
+        verticalFit: false
+    }
+});
